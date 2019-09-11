185602374149c90e47463cb07e2b69a6cd282ad3
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-11 02:47:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 #write your code here
-number = 10
-def countdown(number)
+
+def countdown
+  number = 10
   while number > 0
   puts "#{number} SECONDS!"
   number -= 1
 end
-countdown()
+countdown
